5e086fbc425bfb135f92b47963bd9d05a126cfe0
b241890035 <bataabat905@gmail.com>
2026-06-09 16:09:55 +0800

    created_date user

--- models.py
+++ models.py
@@ -16,6 +16,7 @@ class User(Base):
     id = Column(Integer, primary_key=True, index=True) # Дахин давтагдахгүй ID
     email = Column(String, unique=True, index=True)    # Хэрэглэгчийн мэйл (давхардахгүй)
     hashed_password = Column(String)                   # Нууцалсан нууц үг
+    created_at = Column(DateTime, default=datetime.utcnow) # Бүртгүүлсэн огноо
 
     # Хэрэглэгч болон тэдний ажлуудын хоорондын холбоо (Нэг хэрэглэгч олон ажилтай байж болно)
     tasks = relationship("Task", back_populates="owner")
@@ -70,6 +71,7 @@ class UserCreate(BaseModel):
 class UserResponse(BaseModel):
     id: int
     email: EmailStr
+    created_at: datetime
     
     class Config:
         from_attributes = True # SQLAlchemy моделийг Pydantic руу хөрвүүлэх боломж олгоно
